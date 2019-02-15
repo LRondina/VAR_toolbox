f1614d6ddea1616cb0cfd7b59ffbb25d780f46f5
Luca Rondina <rondina.luc@gmail.com>
2019-02-15 09:58:45 +0000

    Added options for plotting

--- VAR/VARhdplot.m
+++ VAR/VARhdplot.m
@@ -28,6 +28,7 @@ end
 %===============================================
 filename = [VARopt.figname 'HD_'];
 quality = VARopt.quality;
+savefigs = VARopt.savefigs;
 suptitle = VARopt.suptitle;
 pick = VARopt.pick;
 
@@ -48,28 +49,31 @@ end
 
 %% Plot
 %===============================================
-FigSize
+
 for ii=pick:nvars                
+    disp(['Historical decomposition of ' vnames{ii}])
+    figure; FigSize(40,15)
     colormap(parula)
     BarPlot(HD.shock(:,1:nshocks,ii));
     xlim([1 nsteps]);
-    title([vnames{ii}], 'FontWeight','bold','FontSize',10); 
-    % Save
-    FigName = [filename num2str(ii)];
-    if quality 
-        if suptitle==1
-            Alphabet = char('a'+(1:nvars)-1);
-            SupTitle([Alphabet(jj) ') HD of '  vnames{ii}])
+    title([vnames{ii}], 'FontWeight','bold','FontSize',10);
+    legend(vnames)
+    
+    if savefigs
+        % Save
+        FigName = [filename num2str(ii)];
+        if quality
+            if suptitle==1
+                Alphabet = char('a'+(1:nvars)-1);
+                SupTitle([Alphabet(jj) ') HD of '  vnames{ii}])
+            end
+            opt = LegOption; LegSubplot(vnames,opt);
+            set(gcf, 'Color', 'w');
+            export_fig(FigName,'-pdf','-png','-painters')
+        else
+            print('-dpng','-r100',FigName);
+            orient landscape
+            print('-dpdf','-bestfit','-r100',FigName);
         end
-        opt = LegOption; LegSubplot(vnames,opt);
-        set(gcf, 'Color', 'w');
-        export_fig(FigName,'-pdf','-png','-painters')
-    else
-        legend(vnames)
-        print('-dpng','-r100',FigName);
-        print('-dpdf','-r100',FigName);
     end
-    clf('reset');
 end
-
-close all
